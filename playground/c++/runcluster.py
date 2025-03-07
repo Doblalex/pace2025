c42837ad567472ab227d9a353e31fbe90b5761c8
Alexander Dobler <adobler@behemoth.ac.tuwien.ac.at>
2025-03-07 15:27:23 +0100

    cluster

--- playground/c++/runcluster.py
+++ playground/c++/runcluster.py
@@ -3,6 +3,10 @@ import sys
 
 FOLDER = "../../instances/official/ds/exact"
 BINARY = "./build/"
-
+cwd = os.getcwd()
 for file in os.listdir(FOLDER):
     if file.endswith(".gr"):
+        cmd = ["qsub", "-N", file, "-l", "bc4", "-e", "~/error.log", "runscript.sh", "./build/dsexact", os.path.join(FOLDER, file), cwd]
+        cmd = " ".join(cmd)
+        print(cmd)
+        os.system(cmd)
--- playground/c++/runscript.sh
+++ playground/c++/runscript.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-cd $SCRIPT_DIR
+cd $3
 file=$(basename $2)
 $1 < $2 > "./results/${file}.out"
