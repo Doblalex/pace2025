994653c27b41d7016490ec58cdd1efe009babba9
Alexander Dobler <adobler@behemoth.ac.tuwien.ac.at>
2025-03-17 09:17:12 +0100

    cluster

--- playground/c++/runcluster.py
+++ playground/c++/runcluster.py
@@ -2,11 +2,13 @@ import os
 import sys
 
 FOLDER = "../../instances/official/ds/exact"
-BINARY = "./build/"
+BINARY = "./build/dsexact"
+OUTDIR = "resultscpsat"
+
 cwd = os.getcwd()
 for file in os.listdir(FOLDER):
     if file.endswith(".gr"):
-        cmd = ["qsub", "-N", file, "-l", "bc4", "-e", "~/error.log", "runscript.sh", "./build/dsexact", os.path.join(FOLDER, file), cwd]
+        cmd = ["qsub", "-N", file, "-l", "bc4", "-e", "~/error.log", "-pe", "pthreads", "8", "-l", "h_vmem=2G", "runscript.sh", BINARY, os.path.join(FOLDER, file), cwd, OUTDIR]
         cmd = " ".join(cmd)
         print(cmd)
         os.system(cmd)
--- playground/c++/runscript.sh
+++ playground/c++/runscript.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd $3
 file=$(basename $2)
-$1 < $2 > "./results/${file}.out"
+(time $1 < $2) &> "./$4/${file}.out"
